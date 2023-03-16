b75ca8c1cdcb3a21219c746671a7e10652be9980
Tushar Botare <tusharbotare17@gmail.com>
2023-03-16 15:21:14 +0530

    Encapsulation Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-  
+
+    public static void main(String args[]) {
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Tushar");
+
+        String ans = obj.getName();
+
+        System.out.println(ans);
+    }
+
 }
